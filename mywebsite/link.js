83b2783ae5ec80eea96eb97227832cbc36c983b6
juwli-a <julipeont45@gmail.com>
2024-07-17 00:34:40 -0300

    Add files via upload

---
+++ mywebsite/link.js
@@ -0,0 +1,7 @@
+btnSim.addEventListener('click', () => {
+    window.location.href = 'love.html';  // Caminho relativo para a página de amor
+});
+
+btnNao.addEventListener('click', () => {
+    window.location.href = 'error.html';  // Caminho relativo para a página de erro
+});
